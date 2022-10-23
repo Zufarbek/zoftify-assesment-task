14d7c1aeee635ff75ab3524326cc314d567a084d
oqtoshqoratoshgapirmaganqozichoq <epaxaboz@britishpreschool.net>
2022-10-23 18:02:03 +0500

    bug fixed in new post page

--- store/slices/posts.ts
+++ store/slices/posts.ts
@@ -1,4 +1,5 @@
 import { createSlice, current, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
 
 interface CounterState {
   value: []
@@ -12,7 +13,7 @@ export const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    addPost: (state, action: PayloadAction<[]>) => {
+    addPost: (state, action: PayloadAction<any>) => {
       if(typeof action.payload == "object" && !Array.isArray(action.payload)) {
         state.value.push(action.payload)
         console.log("Object", action.payload)
