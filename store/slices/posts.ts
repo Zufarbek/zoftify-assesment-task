4c50efb2d5ccacb3c8a5ff55e00f0e0e68ee56ee
oqtoshqoratoshgapirmaganqozichoq <epaxaboz@britishpreschool.net>
2022-10-23 17:13:41 +0500

    slice post fixed

--- store/slices/posts.ts
+++ store/slices/posts.ts
@@ -1,23 +1,18 @@
 import { createSlice, current, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from '../store'
 
-// Define a type for the slice state
 interface CounterState {
   value: []
 }
 
-// Define the initial state using that type
 const initialState: CounterState = {
   value: [],
 }
 
 export const postsSlice = createSlice({
   name: 'posts',
-  // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    addPost: (state, action: PayloadAction<any>) => {
+    addPost: (state, action: PayloadAction<[]>) => {
       if(typeof action.payload == "object" && !Array.isArray(action.payload)) {
         state.value.push(action.payload)
         console.log("Object", action.payload)
@@ -35,7 +30,6 @@ export const postsSlice = createSlice({
 
 export const { addPost } = postsSlice.actions
 
-// Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.state.value
+export const selectCount = (state: any) => state.state.value
 
 export default postsSlice.reducer
